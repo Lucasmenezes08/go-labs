58b3396ad12b6102748b7e8121371adc915facdd
Lucasmenezes08 <lms4@cesar.school>
2026-04-20 15:17:48 -0300

    add twosum exercise O(n)

---
+++ learnGoWithTests/algorithms/twoSum/twoSum_test.go
@@ -0,0 +1,20 @@
+package algorithms
+
+import (
+	"reflect"
+	"testing"
+)
+
+
+func TestTwoSum (t *testing.T){
+	nums := []int{2,7,11,15}
+	target := 9
+
+	result := twoSum(nums,target)
+	expected := []int{0,1}
+
+	if !reflect.DeepEqual(result , expected) {
+		t.Errorf("esperado %v , resultado %v , dado %v", expected , result , nums)
+	}
+}
+ 
